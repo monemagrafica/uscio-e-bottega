af291a63664bb1961f1e458545e4943a9e0716d3
grafica <paolo@monema.it>
2022-06-28 16:56:41 +0200

    bug framer sulle animazione delle routes-spostati gli stili della search page in global

--- pages/search/index.jsx
+++ pages/search/index.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useContext } from 'react'
-import style from './search.module.scss'
+
 import { motion } from 'framer-motion'
 import { animateSearchPage } from '../../components/animations'
 import { BiArrowBack } from 'react-icons/bi'
@@ -29,15 +29,15 @@ console.log(prodotti);
   }
 
   return (
-    <motion.main className={style.search}
+    <motion.main className="search"
       initial='initial'
       animate='animate'
       variants={animateSearchPage}
     >
-      <div className={style.headerRicerca}><button className="close" onClick={() => router.back()}><BiArrowBack /></button> <h1>Ricerca</h1></div>
-      <h2 className={style.subSearch}>Ricerca per nome o ingrediente</h2>
+      <div className="headerRicerca"><button className="close" onClick={() => router.back()}><BiArrowBack /></button> <h1>Ricerca</h1></div>
+      <h2 className="subSearch">Ricerca per nome o ingrediente</h2>
       <label htmlFor="search" >
-        <input onChange={() => risultatiRicerca(inputRicerca.current.value)} ref={inputRicerca} type="text" className={style.searchInput} id="search" placeholder='cerca' />
+        <input onChange={() => risultatiRicerca(inputRicerca.current.value)} ref={inputRicerca} type="text" className="searchInput" id="search" placeholder='cerca' />
       </label>
       {(filtroRicerca.length > 0) && <div className={style.wrapperListaSearch}>
         {filtroRicerca?.map((item) => {
