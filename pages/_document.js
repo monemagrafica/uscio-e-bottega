2bce9138322063ad7ed82cba673be2e1314d31be
grafica <paolo@monema.it>
2022-07-14 13:23:52 +0200

    manifes con indirizzo

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,8 @@ render(){
         <Html>
             <Head>
                 <link rel="manifest" href="/manifest.json" />
+                <meta name="theme-color" content="#D4C9A7"/>
+                <link rel="apple-touch-icon" href="/images/icon-512x512.png"></link>
             </Head>
             <body>
                 <Main />
