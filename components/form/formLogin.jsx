981ba092b3870bf4336e8f514b2ca1d9ce3476f7
grafica <paolo@monema.it>
2023-10-18 17:41:46 +0200

    google login funzionante

--- components/form/formLogin.jsx
+++ components/form/formLogin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, use } from 'react'
 import { useRouter } from 'next/router'
 import { motion, AnimatePresence } from 'framer-motion'
 import { animateLogin } from '../utils/animations'
@@ -7,13 +7,10 @@ import { getAuth } from 'firebase/auth';
 import { auth } from '../../firebase/initFirebase';
 import style from './form.module.scss'
 import { FcGoogle } from 'react-icons/fc'
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-
-
-const googleAuth = auth
+import { useAuth } from '../../context/authContext';
 
 function FormLogin({ auth, formAuth, setFormAuth }) {
-
+  const { loginGoogle } = useAuth()
   const router = useRouter()
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
@@ -27,33 +24,6 @@ function FormLogin({ auth, formAuth, setFormAuth }) {
 
 
 
-  const provider = new GoogleAuthProvider();
-  const loginGoogle = () => {
-    signInWithPopup(googleAuth, provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential?.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        if (user) { router.push('/store') }
-
-
-        console.log({ credential, token, user });
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        console.log({ errorCode, errorMessage, email, credential });
-      });
-    console.log('gugol');
-  };
 
 
 
--- context/authContext.jsx
+++ context/authContext.jsx
@@ -1,12 +1,42 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { signInWithPopup, signOut, onAuthStateChanged, GoogleAuthProvider } from "firebase/auth";
 import { auth } from '../firebase/initFirebase';
 
-const ContextAuth = ({ children }) => {
-    const AuthContext = createContext();
+const AuthContext = createContext();
 
+const ContextAuth = ({ children }) => {
+
+
+    const provider = new GoogleAuthProvider();
+
+    const loginGoogle = () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential?.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                if (user) { router.push('/store') }
+
+
+                console.log({ credential, token, user });
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                console.log({ errorCode, errorMessage, email, credential });
+            });
+        console.log('gugol');
+    };
     return (
-        <AuthContext.Provider authData={{ name: "pippo" }}>
+        <AuthContext.Provider value={{ loginGoogle: loginGoogle }}>
             {children}
         </AuthContext.Provider>
     );
