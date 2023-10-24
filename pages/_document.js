0e89303952c9fd3dd6b5a3751e5844c37e364762
grafica <paolo@monema.it>
2023-10-24 10:37:22 +0200

    modificato lo start_url

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,12 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#D4C9A7" />
           <link rel="apple-touch-icon" href="/images/icon-512x512.png"></link>
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Uscio e Bottega" />
+          <meta property="og:description" content="Uscio e Bottega app" />
+          <meta property="og:site_name" content="Uscio e Bottega" />
+          <meta property="og:url" content="https://yourdomain.com" />
+          <meta property="og:image" content="/public/images/icona-pwa.png" />
         </Head>
         <body>
           <Main />
