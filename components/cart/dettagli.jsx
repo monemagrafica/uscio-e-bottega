0db5b1e00383c383e21b8b4e6ab37078feb6f15e
grafica <paolo@monema.it>
2023-10-25 13:28:03 +0200

    migliorata interfaccia cart

--- components/cart/dettagli.jsx
+++ components/cart/dettagli.jsx
@@ -49,37 +49,47 @@ function Dettagli({ salseLista, arrayFromSalse, dettagliOpen, immagine, idAddedP
             changeSalse(idAddedPanino, [...salseLista, salsa])
         }
     }
-    console.log(salseLista, 'salseLista')
+    console.log(arrayFromSalse, 'arrayFromSalse')
 
 
     return (
         <div className={style.dettagli}>
             <div className={`${[style.wrapperDettagli, dettagliOpen && style.dettagliOpen].join(' ')}`}>
-                <div className={style.immagineDettagli}>
+                {/*              <div className={style.immagineDettagli}>
                     <Image src={immagine} width={500} height={300} alt="immagine panino" />
-                </div>
-                {arrayFromSalse && <ul>
-                    {arrayFromSalse.map((item) => {
-                        const presenteInCart = salseLista.includes(item)
-                        return (
-                            <li key={item}>
-                                <form>
-                                    <input
-                                        type="checkbox"
-                                        defaultChecked={presenteInCart}
-                                        id={`${idAddedPanino}___${item}`}
-                                        name={item}
-                                        value={item}
-                                        onClick={(e) => checkboxSalse(e.target.checked, e.target.value)}
-                                    />
-                                    <label htmlFor={`${idAddedPanino}___${item}`}>
-                                        {item}
-                                    </label><br></br>
-                                </form>
-                            </li>
-                        )
-                    })}
-                </ul>}
+                </div> */}
+                {arrayFromSalse.length ?
+                    <div className={style.wrapperSalse}>
+                        <div className={style.titleSalse}>Seleziona le salse:</div>
+                        <div className={style.boxSx} >
+                            <Image src={'/images/salsa.svg'} width={45} height={35} alt="immagine panino" />
+                        </div>
+                        <div className={style.boxDx}>
+
+                            <ul>
+                                {arrayFromSalse.map((item) => {
+                                    const presenteInCart = salseLista.includes(item)
+                                    return (
+                                        <li key={item}>
+                                            <form>
+                                                <input
+                                                    type="checkbox"
+                                                    defaultChecked={presenteInCart}
+                                                    id={`${idAddedPanino}___${item}`}
+                                                    name={item}
+                                                    value={item}
+                                                    onClick={(e) => checkboxSalse(e.target.checked, e.target.value)}
+                                                />
+                                                <label htmlFor={`${idAddedPanino}___${item}`}>
+                                                    {item}
+                                                </label><br></br>
+                                            </form>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
+                    </div> : null}
                 <div className={style.noteDettagli}>
                     <h3>note</h3>
                     <textarea onChange={(e) => setNote(idAddedPanino, e.target.value)} defaultValue={noteCart} name="note" id="note" cols="30" rows="5"></textarea>
